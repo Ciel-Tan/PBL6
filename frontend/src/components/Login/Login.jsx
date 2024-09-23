b7a6e555fa246e551c9b690ea436e27cd6fbced1
Ciel Tan <ngoduytan21072003@gmail.com>
2024-09-23 22:13:12 +0700

    validate login page and fix some issues, warnings

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -3,53 +3,82 @@ import "./util.css"
 import "../fonts/font-awesome-4.7.0/css/font-awesome.min.css"
 import "../fonts/iconic/css/material-design-iconic-font.min.css"
 
+import { useLoginFormik } from "../validate/Formik"
+
+
 const Login = () => {
+    const formik = useLoginFormik()
+
     return ( 
         <div className="limiter">
             <div className="container-login100" style={{ backgroundImage: `url('/images/bg-01.jpg')` }}>
                 <div className="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
-                <form className="login100-form validate-form">
+                <form className="login100-form validate-form" onSubmit={formik.handleSubmit}>
                     <span className="login100-form-title p-b-49">Login</span>
 
                     <div className="wrap-input100 validate-input m-b-23" data-validate="Username is required">
-                    <span className="label-input100">Email or username</span>
-                    <input className="input100" type="text" name="username" placeholder="Type your email or username" />
-                    <span className="focus-input100" data-symbol="&#xf206;"></span>
+                        <span className="label-input100">Email or username</span>
+                        <input 
+                            className="input100" 
+                            type="text" 
+                            id="username"
+                            name="username"
+                            onChange={formik.handleChange}
+                            placeholder="Type your email or username"
+                            autoComplete="username"
+                        />
+                        <span className="focus-input100" data-symbol="&#xf206;"></span>
                     </div>
+
+                    {formik.errors.username && (
+                        <p className="errorMsg">{formik.errors.username}</p>
+                    )}
 
                     <div className="wrap-input100 validate-input" data-validate="Password is required">
-                    <span className="label-input100">Password</span>
-                    <input className="input100" type="password" name="pass" placeholder="Type your password" />
-                    <span className="focus-input100" data-symbol="&#xf190;"></span>
+                        <span className="label-input100">Password</span>
+                        <input 
+                            className="input100" 
+                            type="password"
+                            id="password"
+                            name="password"
+                            onChange={formik.handleChange}
+                            placeholder="Type your password"
+                            autoComplete="current-password"
+                        />
+                        <span className="focus-input100" data-symbol="&#xf190;"></span>
                     </div>
 
+                    {formik.errors.password && (
+                        <p className="errorMsg">{formik.errors.password}</p>
+                    )}
+
                     <div className="text-right p-t-8 p-b-31">
-                    <a href="#">Forgot password?</a>
+                        <a href="/forgotPassword">Forgot password?</a>
                     </div>
 
                     <div className="container-login100-form-btn">
-                    <div className="wrap-login100-form-btn">
-                        <div className="login100-form-bgbtn"></div>
-                        <button className="login100-form-btn">Login</button>
-                    </div>
+                        <div className="wrap-login100-form-btn">
+                            <div className="login100-form-bgbtn"></div>
+                            <button className="login100-form-btn" type="submit">Login</button>
+                        </div>
                     </div>
 
-                    <div class="login-container">
-                        <p class="sign-up-text">Don't have an account? <a href="#">Sign up</a></p>
+                    <div className="login-container">
+                        <p className="sign-up-text">Don't have an account? <a href="/signup">Sign up</a></p>
 
-                        <div class="divider">
-                            <span class="line"></span>
-                            <span class="or-text">Or continue with</span>
-                            <span class="line"></span>
+                        <div className="divider">
+                            <span className="line"></span>
+                            <span className="or-text">Or continue with</span>
+                            <span className="line"></span>
                         </div>
 
-                        <div class="social-login">
-                            <button class="social-btn">
-                                <img src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg" alt="Google" class="icon" />
+                        <div className="social-login">
+                            <button className="social-btn">
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg" alt="Google" className="icon" />
                                 Sign in with Google
                             </button>
-                            <button class="social-btn">
-                                <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="Facebook" class="icon" />
+                            <button className="social-btn">
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="Facebook" className="icon" />
                                 Sign in with Facebook
                             </button>
                         </div>
@@ -57,7 +86,7 @@ const Login = () => {
                 </form>
                 </div>
             </div>
-            </div>
+        </div>
     );
 }
  
